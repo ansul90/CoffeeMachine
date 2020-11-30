5585b6aaf1799a03b16186ea43c043c38687c9f5
Ansul Gupta <ansul.gupta@olacabs.com>
2020-11-30 17:40:35 +0530

    Updated comments

--- src/CoffeeMachineDriver.java
+++ src/CoffeeMachineDriver.java
@@ -1,7 +1,6 @@
 import services.CoffeeMachineService;
 
 public class CoffeeMachineDriver {
-
     public static void main(String[] args) {
         CoffeeMachineService coffeeMachineService = new CoffeeMachineService();
         String url = "https://api.npoint.io/c86888c679448e8831c7";
--- src/services/CoffeeMachineService.java
+++ src/services/CoffeeMachineService.java
@@ -26,6 +26,7 @@ public class CoffeeMachineService {
         }
     }
 
+    // Reads JSON from URL and pass the DAO to the process function
     public void readDataFromURL(String url) {
         ObjectMapper objectMapper = new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         try {
@@ -38,25 +39,34 @@ public class CoffeeMachineService {
         }
     }
 
+    /* Processes the input JSON and use a combination of decorator & singleton design pattern
+    Initialize the coffee machine with initial available units
+    Validates whether item is available and if available is the quantity enough
+    */
     private void process(InputDao machineDao) {
         initializeMachine(machineDao.getMachine().getTotalItemsQuantity());
         Map<String, Map<String, Integer>> beveragesList = machineDao.getMachine().getBeverages();
         boolean flag = true;
+        // Get beverages from input and parse the contents
         for (Map.Entry<String, Map<String, Integer>> hM : beveragesList.entrySet()) {
             Map<String, Integer> contentsMap = hM.getValue();
             List<String> remaining = new ArrayList<>();
             List<String> unavailableList = new ArrayList<>();
+            // Parse contents for a beverage
             for (Map.Entry<String, Integer> content : contentsMap.entrySet()) {
                 BeverageDecorator beverageDecorator = contentFactory.getContent(content.getKey());
+                // if item is available
                 if (!availableItems.contains(content.getKey())) {
                     unavailableList.add(content.getKey());
                     flag = false;
                     break;
+                    // if item quantity is enough
                 } else if (!isPossible(beverageDecorator, content.getValue())) {
                     flag = false;
                     remaining.add(content.getKey());
                 }
             }
+            // updating the quantity of the content
             if (flag) {
                 for (Map.Entry<String, Integer> content : contentsMap.entrySet()) {
                     BeverageDecorator beverageDecorator = contentFactory.getContent(content.getKey());
@@ -74,10 +84,12 @@ public class CoffeeMachineService {
         }
     }
 
+    // Verifies whether the quantity is available
     private boolean isPossible(BeverageDecorator beverageDecorator, int requiredQuantity) {
         return beverageDecorator.getTotalQuantity() >= requiredQuantity;
     }
 
+    // Initialise machine with total quantity available
     private void initializeMachine(Map<String, Integer> totalContents) {
         for (Map.Entry<String, Integer> content : totalContents.entrySet()) {
             BeverageDecorator beverageDecorator = contentFactory.getContent(content.getKey());
